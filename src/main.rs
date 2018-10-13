225857933bc0b52a31169df40a293c950b1a9e4b
jarryliu <jarrysix@gmail.com>
2018-10-13 12:12:07 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate clap;
+extern crate iron;
+
+use clap::{App, Arg};
+use iron::prelude::Iron;
+include!("lib.rs");
+
+fn main() {
+    let args = [
+        Arg::with_name("conf").short("c")
+            .takes_value(true).default_value("./"),
+        Arg::with_name("port").short("p")
+            .takes_value(true).default_value("8302"),
+        Arg::with_name("debug").short("d")
+            .takes_value(false),
+    ];
+    let matches = App::new("jrd").args(&args).get_matches();
+    let conf = matches.value_of("conf").unwrap();
+    let debug = matches.is_present("debug");
+    let _port = matches.value_of("port").unwrap();
+    println!("conf is {:?}", conf);
+    println!("conf is {:?}", debug);
+
+    let mut addr: String = "0.0.0.0:".to_string() + _port;
+    let entry = jrd::Entry::new(conf.to_string(), debug);
+    //println!("{:#?}",entry::req_entry);
+    let _server = Iron::new(entry).http(addr).unwrap();
+    println!("serve on port {}", _port);
+}
+
---
+++ src/main2.rs
@@ -0,0 +1,26 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+fn main() {
+    println!("Hello, world!");
+    guest_game();
+}
+
+fn guest_game(){
+    println!("Guess the number");
+
+    let mut guess = String::new();
+    let rdn =rand::thread_rng().gen_range(1,100);
+    println!("Please input your guess , luck number is {}",rdn);
+    loop {
+        io::stdin().read_line(&mut guess)
+            .expect("input a incorrect guess");
+        let guess: i32 = guess.trim().parse()
+            .expect("please input a number");
+        let b = guess == rdn;
+        println!("your guess is {}", b);
+        if b {
+            break;
+        }
+    }
+}
