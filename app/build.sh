af11317ab01964a7a6358a5159212ccab928facf
jarryliu <jarrysix@gmail.com>
2019-01-07 15:33:37 +0800

    static app

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 NODE_ENV=production
-rm -rf ./dist && rm -rf ../static/app
-parcel build --no-cache --public-url ./ index.html
-cp -r dist ../static/app
+rm -rf ./dist && rm -rf ../static/app && \
+    parcel build --no-cache --public-url ./ index.html && \
+    cp -r dist ../static/app && \
+    find ../static/app -name "*.map"|xargs rm -rf
--- build-static-app.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-yarn add parcel-bundler --dev
-yarn add react react-dom
