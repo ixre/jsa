b7be310fe7c72f2187b2275fc3d11afb0e6bfea2
jarryliu <jarrysix@gmail.com>
2019-01-07 16:38:55 +0800

    feature

--- app/index.js
+++ app/index.js
@@ -1,40 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./src/css/page.less";
-const Loading = props => <div>{props.text}</div>;
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            child: <Loading text={"click load"}/>,
-            routes:<div></div>
+            routes:<span></span>
         };
     }
-
     componentDidMount() {
-        import("./src/routes").then(({IndexRouters}) => {
+        import("./src/features/home/main").then(({IndexRouters}) => {
             this.setState({
                 routes: <IndexRouters/>
             });
         });
     }
 
-    //动态导入
-    dynImport() {
-        import("./src/load").then(({LazyLoad}) => {
-            this.setState({
-                child: <LazyLoad/>
-            });
-        });
-    };
-
     render() {
         return (
-            <div>
-                <div onClick={this.dynImport.bind(this)}>
-                    <button>click me</button>
-                    {this.state.child}
-                </div>
+            <div className="app-container">
                 {this.state.routes}
             </div>
         );
--- app/src/features/home/main.js
+++ app/src/features/home/main.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
-import Main from './main';
-import Login from "./login"
+import React from "react";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "../login/login";
+
+
+export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return <div>...</div>;
+    }
+}
+
 
 export class IndexRouters extends React.Component {
     render() {
