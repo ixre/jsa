23932c4827a061a27a0e927289a164f453b8595c
jarryliu <jarrysix@gmail.com>
2019-01-19 16:29:38 +0800

    tests

---
+++ tests/mutex.rs
@@ -0,0 +1,45 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::RwLock;
+use std::thread;
+
+#[test]
+fn mutex() {
+    let x = Mutex::new(0);
+    let h = thread::spawn(move || {
+        let mut s = x.lock().unwrap();
+        *s = 1;
+    });
+    h.join().unwrap();
+    //println!("{:#?}",x.lock().unwrap());
+}
+
+#[test]
+fn arc_mutex() {
+    let global = Arc::new(Mutex::new(0));
+    let mut threads = vec![];
+    for i in 0..5 {
+        let clone = global.clone();
+        threads.push(thread::spawn(move || {
+            let mut s = clone.lock().unwrap();
+            *s += 1;
+        }));
+    }
+    threads.into_iter().for_each(|t| { t.join(); });
+    println!("{:?}", *global.lock().unwrap())
+}
+
+#[test]
+fn rw_lock() {
+    let global = Arc::new(RwLock::new(0));
+    let mut threads = vec![];
+    for i in 0..5 {
+        let clone = global.clone();
+        threads.push(thread::spawn(move || {
+            let mut s = clone.write().unwrap();
+            *s += 1;
+        }));
+    }
+    threads.into_iter().for_each(|t| { t.join(); });
+    println!("{:?}", *global.read().unwrap())
+}
